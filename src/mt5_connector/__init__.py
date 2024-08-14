f5d06a5a8e8464b5ede7e959c01271706e445327
Thibault delrieu <thibault.delrieu.pro@gmail.com>
2024-08-14 15:09:31 +0200

    add enum to init file for library implemeentation

--- src/mt5_connector/__init__.py
+++ src/mt5_connector/__init__.py
@@ -5,3 +5,5 @@ from mt5_connector.connector.market_data import get_data, get_current_tick
 from mt5_connector.connector.trade import TradeManagement
 
 from mt5_connector.tools.dataclass_definition import TradeObject, Candle, Tick
+
+from mt5_connector.tools.global_object import PendingOrder, DirectOrder, Order, TradeRequestActions, OrderTypeFilling, OrderTypeTime
